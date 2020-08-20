edad5d0ccba96727fd8290b40cf2696863a864ec
marvin000 <marvin000@snu.ac.kr>
2020-08-20 20:55:20 +0900

    feature: 로그인 api 구현

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,5 +1,6 @@
 const pool = require('../utils/mysql')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 async function userRegister(req, res, next) {
@@ -17,7 +18,7 @@ async function userRegister(req, res, next) {
     const query = 'INSERT INTO test.user (name, email, password) VALUES (?,?,?)'
     await connection.query(query, [name, email, hashedPassword])
     res.status(200).json({
-      message: 'register success'
+      message: 'register success',
     })
   } catch (err) {
     next(err)
@@ -25,7 +26,31 @@ async function userRegister(req, res, next) {
     connection.release()
   }
 }
+async function login(req, res, next) {
+  const { email, password } = req.body
+  if (!email || !password) {
+    next(new Error('Bad Request'))
+  }
+  const connection = await pool.getConnection()
+  try {
+    const query = 'SELECT id, email, password FROM test.user WHERE email=?'
+    const [users] = await connection.query(query, [email])
+    if (!users) next(new Error('Not registered'))
 
+    const user = users[0]
+    const isValid = await bcrypt.compare(password, user.password)
+    if (!isValid) next(new Error('Invalid Password'))
+
+    const payload = { id: user.id }
+    const token = await jwt.sign(payload, process.env.JWT_SECRET)
+    res.status(200).json({ token })
+  } catch (err) {
+    next(err)
+  } finally {
+    connection.release()
+  }
+}
 module.exports = {
   userRegister,
+  login,
 }
