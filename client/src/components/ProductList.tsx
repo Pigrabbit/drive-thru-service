71037a6614455f407a94b28f3d0131e85d9ced87
Pigrabbit <donghyuk1003@gmail.com>
2020-09-17 21:23:22 +0900

    feat: modal visibility에 redux 적용

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -1,5 +1,7 @@
 import React, { useState, MouseEvent } from 'react'
 import styled from 'styled-components'
+import { TOGGLE_MODAL } from '../app/actionTypes'
+import { store } from '../app/store'
 import { ProductType } from '../pages/StorePage'
 import Modal from './Modal'
 import ProductRow from './ProductRow'
@@ -20,24 +22,31 @@ export default function ProductList(props: Props) {
   const clickProductHandler = (e: MouseEvent) => {
     const id = e.currentTarget.id.split('-')[1]
     setClickedProductId(parseInt(id))
-    setIsModalVisible(true)
+
+    store.dispatch({ type: TOGGLE_MODAL, payload: { isVisible: true } })
+    const { isVisible } = store.getState()
+
+    setIsModalVisible(isVisible)
   }
 
   const clickModalOutsideHandler = (e: MouseEvent<HTMLDivElement>) => {
-    if (!(e.target as HTMLDivElement).classList.contains("modal-overlay")) return
-    setIsModalVisible(false)
+    if (!(e.target as HTMLDivElement).classList.contains('modal-overlay')) return
+    store.dispatch({ type: TOGGLE_MODAL, payload: { isVisible: false } })
+    const { isVisible } = store.getState()
+
+    setIsModalVisible(isVisible)
   }
 
   return (
     <StyledContainer className="product-list">
       {productList.length > 0 ? (
         productList.map((product, idx) => (
-          <ProductRow key={idx} product={product} onClick={clickProductHandler}/>
+          <ProductRow key={idx} product={product} onClick={clickProductHandler} />
         ))
       ) : (
         <p>준비중입니다...</p>
       )}
-      {(clickedProductId > 0 && isModalVisible)? (
+      {clickedProductId > 0 && isModalVisible ? (
         <Modal
           product={productList.filter((product) => product.id === clickedProductId)[0]}
           clickModalOutsideHandler={clickModalOutsideHandler}
