7efd76a2073a54564602ee12503335933e1f085e
Pigrabbit <donghyuk1003@gmail.com>
2020-09-23 20:57:25 +0900

    config: server build 수정

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ require('dotenv').config({
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
 app.use(cors())
 app.use(morgan(logFormat))
 
@@ -21,7 +22,7 @@ const apiRouter = require('./routes/api-router')
 app.use('/api', apiRouter)
 
 app.use('/', (req, res, next) => {
-  res.json({ message: 'hello world' })
+  res.sendFile('public/index.html', { root: __dirname })
 })
 
 app.use((err, req, res, next) => {
